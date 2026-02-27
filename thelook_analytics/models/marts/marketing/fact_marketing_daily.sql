5a9adcbe625496337e39fade5dded1b14a806c97
marta-bilinska-gen-tech <157017420+marta-bilinska-gen-tech@users.noreply.github.com>
2026-02-28 01:02:29 +0200

    remove answers

--- thelook_analytics/models/marts/marketing/fact_marketing_daily.sql
+++ thelook_analytics/models/marts/marketing/fact_marketing_daily.sql
@@ -1,25 +1,12 @@
-{{
-  config(
-    materialized='table',
-    partitions={'field': 'date', 'data_type': 'date'}
-  )
-}}
+{{ config(materialized='table') }}
 
-with sessions as (
-  select session_id, session_date as date from {{ ref('int_event_sessions') }}
-),
-events as (
-  select * from {{ ref('stg_events') }}
+with source_data as (
+
+    select 1 as id
+    union all
+    select null as id
+
 )
 
-select
-  s.date,
-  count(distinct e.user_id) as active_users,
-  count(distinct s.session_id) as sessions,
-  sum(case when e.event_type = 'view' then 1 else 0 end) as product_views,
-  sum(case when e.event_type = 'add_to_cart' then 1 else 0 end) as add_to_carts,
-  sum(case when e.event_type = 'purchase' then 1 else 0 end) as purchases,
-  safe_divide(sum(case when e.event_type = 'purchase' then 1 else 0 end), nullif(sum(case when e.event_type = 'view' then 1 else 0 end),0)) as conversion_rate
-from sessions s
-left join events e on s.session_id = e.session_id
-group by s.date
+select *
+from source_data
